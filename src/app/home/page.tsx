dc7b853f41717d922b77ad2c932164c87cf8be7d
Nam Khuc <khucphuongnam2005@gmail.com>
2025-06-18 02:45:48 +0700

    ADD:Unfinished NavBar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,10 @@
 import SideBar from "@/components/SideBar/SideBar";
-
+import NavBar from "@/components/NavBar/NavBar";
 export default function page() {
   return (
     <div className="flex">
       <SideBar />
+      <NavBar />
       <p>Nội dung trang chủ</p>
     </div>
   );
